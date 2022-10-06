5b53f905cf2e95944deb45cb837e8095b889aa81
StevenMad <stevenmadec@live.fr>
2022-10-07 01:37:19 +0200

    2nd commit

---
+++ components/Hero.jsx
@@ -0,0 +1,31 @@
+export default function Example() {
+  return (
+    <div className="relative overflow-hidden bg-gray-50">
+      <div className="relative pt-6 pb-16 sm:pb-24">
+        <main className="mx-auto mt-24 max-w-7xl px-4 sm:mt-32">
+          <div className="text-center">
+            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
+              Beautiful Tailwindcss ecommerce{" "}
+            </h1>
+            <p className="mx-auto mt-3 max-w-md text-base text-gray-500 sm:text-lg md:mt-5 md:max-w-3xl md:text-xl">
+              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
+              lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
+              fugiat aliqua.
+            </p>
+            <div className="mx-auto mt-5 max-w-md sm:flex sm:justify-center md:mt-8">
+              <div className="rounded-md shadow">
+                <a
+                  href="#"
+                  className="flex w-full items-center justify-center rounded-md border border-transparent bg-gray-900 divide-x divide-fray-600 px-6 py-4 text-base font-medium text-white hover:bg-gray-700 md:text-lg"
+                >
+                <span className="pr-6">Get the Bundle</span>
+                <span className="pl-6">$199</span>
+                </a>
+              </div>
+            </div>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+}
